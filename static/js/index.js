9c35f7b8963aded8830e44d1535084c13050722d
Richo <richo2192000@gmail.com>
2016-01-31 18:19:40 +0800

    Change structure.

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,33 @@
-window.onload = function() {
+var currentCountry;
+
+var createMap = function(){
+  return new Datamap({
+    element: document.getElementById("worldmap"),
+    geographyConfig: {
+      highlightBorderColor: '#bada55',
+      popupTemplate: popupFunc,
+      highlightBorderWidth: 3
+    },
+    fills: {
+      defaultFill: "#ABDDA4",
+      residence: "#fa0fa0",
+      TWN: "#0000FF"
+    },
+    data: {
+      TWN: { fillKey: "TWN" }
+    }
+  });
+};
+
+var popupFunc = function(geography, data){
+  if(currentCountry!=geography.id){
+    currentCountry = geography.id;
+    drawLine(geography);
+  }
+  return '<div class="hoverinfo">' + geography.properties.name
+};
+
+var drawLine = function(geography){
+  if(!(geography.id in travelData))
+    return;
 };
