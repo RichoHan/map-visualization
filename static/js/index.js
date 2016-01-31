e7dbd61d85fe8365f26fff9ff0d2e5a08e564c64
Richo <richo2192000@gmail.com>
2016-01-31 18:46:11 +0800

    Visualizing line charts.

--- static/js/index.js
+++ static/js/index.js
@@ -22,12 +22,22 @@ var createMap = function(){
 var popupFunc = function(geography, data){
   if(currentCountry!=geography.id){
     currentCountry = geography.id;
-    drawLine(geography);
+    drawLineChart(geography);
   }
   return '<div class="hoverinfo">' + geography.properties.name
 };
 
-var drawLine = function(geography){
+var drawLineChart = function(geography){
   if(!(geography.id in travelData))
     return;
+  console.log("Lines");
+  var history = {
+    2009: travelData[geography.id]["2009"],
+    2010: travelData[geography.id]["2010"],
+    2011: travelData[geography.id]["2011"],
+    2012: travelData[geography.id]["2012"],
+    2013: travelData[geography.id]["2013"],
+    2014: travelData[geography.id]["2014"]
+  };
+  new Chartkick.LineChart("chart-1", history);
 };
